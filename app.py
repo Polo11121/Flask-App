dcaefe2f5285cda2c38ecf927a6ce63e1d5abf1d
michal <michal.jasinski11121@gmail.com>
2023-04-04 18:50:15 +0200

    fourth commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ s3 = boto3.client('s3', aws_access_key_id=os.environ.get('ACCESS_KEY'), aws_secr
 
 app = Flask(__name__)
 app.config['S3_BUCKET_NAME'] = os.environ.get('BUCKET_NAME')
-app.secret_key = 'your_secret_key'
+app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
